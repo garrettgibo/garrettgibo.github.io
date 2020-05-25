856fdbd8417ccd92d4d71fa36bc1e4bc823c4123
Garrett Gibo <garrett.gibo@gmail.com>
2020-05-25 00:57:00 -0700

    cleaning

--- render.py
+++ render.py
@@ -1,8 +1,10 @@
 """Render website templates with jinja"""
 
+
 def render():
     # TODO render page with templates
     pass
+
 
 if __name__ == "__main__":
     pass
--- tests/test_fake.py
+++ tests/test_fake.py
@@ -1,5 +1,6 @@
 """Test python rendering"""
 # TODO test render function
 
+
 def test_nothing():
     pass
