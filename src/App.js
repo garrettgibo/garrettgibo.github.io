f509e5f215c5c0dbb5799c68b9494433030da93c
Garrett Gibo <garrett.gibo@gmail.com>
2020-06-30 00:36:39 -0700

    Mimic old content with react components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import myDogs from './images/kuma_mimi.jpg';
+import githubLogo from './images/toppng.com-github-logo-png-2000x665.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="h-center">
+      <h1>Hi, I'm Garrett</h1>
+      <p>One day I will fill this in with all my projects, passions, and pursuits,
+      but for now enjoy this picture of my dogs</p>
+      <div>
+        <img src={myDogs} alt="My dogs Kuma and Mimi" />
+      </div>
+
+      I do some work here and there, so check out what I do
+      <div>
+        <a href="https://github.com/garrettgibo" target="_blank">
+          <img src={githubLogo} alt="Github" />
         </a>
-      </header>
+      </div>
     </div>
   );
 }
