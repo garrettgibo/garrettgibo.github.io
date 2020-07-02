cc978ce83fe61993c734075ff9d22dbf7d806635
Garrett Gibo <garrett.gibo@gmail.com>
2020-07-01 17:50:49 -0700

    Updates

---
+++ precache-manifest.7e70605e274f71941eb36264d12f836b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edab9f4108045b6395a3689cf483eaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28960c6d2753d925cbce",
+    "url": "/static/css/main.666b0a5c.chunk.css"
+  },
+  {
+    "revision": "405a4d2fc6deeaaa58f6",
+    "url": "/static/js/2.c9e9a509.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c9e9a509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28960c6d2753d925cbce",
+    "url": "/static/js/main.210adfe3.chunk.js"
+  },
+  {
+    "revision": "710d9e47abdd6169265a",
+    "url": "/static/js/runtime-main.fafc0f5c.js"
+  },
+  {
+    "revision": "2acca187aa659e255d838741d1a6239a",
+    "url": "/static/media/kuma_mimi.e5945369.jpg"
+  },
+  {
+    "revision": "e73d629170d7279239223b438e084343",
+    "url": "/static/media/toppng.com-github-logo-png-2000x665.57db8f70.png"
+  }
+]);
