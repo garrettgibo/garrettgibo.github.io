938ac906699e043d5d159b42b3c5eb834a8e43e1
Garrett Gibo <garrett.gibo@gmail.com>
2020-09-16 21:55:34 -0700

    Revert to defaults for super-linter (#21)
    * Revert to defaults for super-linter
    
    * Ignore generated logs
    
    * Fix super-linter warnings

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import './css/App.css';
-import myDogs from './images/kuma_mimi.jpg';
-import githubLogo from './images/toppng.com-github-logo-png-2000x665.png';
+import React from 'react'
+import './css/App.css'
+import myDogs from './images/kuma_mimi.jpg'
+import githubLogo from './images/toppng.com-github-logo-png-2000x665.png'
 
-function App() {
+function App () {
   return (
-    <div className="h-center">
+    <div className='h-center'>
       <h1>Hi, I'm Garrett</h1>
-      <p>One day I will fill this in with all my projects, passions, and pursuits,
-      but for now enjoy this picture of my dogs</p>
+      <p>
+      One day I will fill this in with all my projects, passions, and pursuits,
+      but for now enjoy this picture of my dogs
+      </p>
       <div>
-        <img src={myDogs} alt="My dogs Kuma and Mimi" />
+        <img src={myDogs} alt='My dogs Kuma and Mimi' />
       </div>
 
+      <h2>
       I do some work here and there, so check out what I do
-      <div>
-        <a href="https://github.com/garrettgibo"
-           target="_blank"
-           rel="noopener noreferrer">
-          <img src={githubLogo} alt="Github" />
-        </a>
-      </div>
+      </h2>
+      <a href='https://github.com/garrettgibo' target='_blank' rel='noopener noreferrer'>
+        <img src={githubLogo} alt='Github Logo' />
+      </a>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
