c45f9a3583ae5734d445412e737223c137ce5354
Garrett Gibo <garrett.gibo@gmail.com>
2020-10-20 15:19:56 -0700

    Fix styling

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import IconRow from './components/IconRow'
 import './css/App.css'
 
 export default function App () {
-    return (
+  return (
     <div className='parent'>
-	<div className='child'>
-	    <h1>Hi I'm Garrett</h1>
-	    <IconRow />
-	</div>
+      <div className='child'>
+	<h1>Hi I'm Garrett</h1>
+	<IconRow />
+      </div>
     </div>
-    )
+  )
 }
