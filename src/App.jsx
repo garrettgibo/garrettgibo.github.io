af2a495312e482b04ad7c1ae4992dea7f76434ff
Garrett Gibo <22530919+garrettgibo@users.noreply.github.com>
2023-03-03 13:05:19 -0800

    Convert to vite (#26)
    * Update to yarn + vite; Fix pathing in index.html for vite
    
    * Update github actions
    
    * Fix actions styling
    
    * Trunk formatting changes

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import IconRow from "./components/IconRow";
+import "./css/App.css";
+
+export default function App() {
+  return (
+    <div className="parent">
+      <div className="child">
+        <h1>Hi I'm Garrett</h1>
+        <IconRow />
+      </div>
+    </div>
+  );
+}
